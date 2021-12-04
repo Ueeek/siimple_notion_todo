ebc691475d8bff5967d7fdf4bc763dc19ca1ffa6
Ueeek <ueki.k.ae@m.titech.ac.jp>
2021-12-04 20:05:50 +0900

    fix

--- rplugin/python3/todo_manager/__init__.py
+++ rplugin/python3/todo_manager/__init__.py
@@ -2,11 +2,11 @@ from notion.client import NotionClient
 from notion.block import TodoBlock
 from notion.block import PageBlock
 import json
-import pynvim
+import neovim
 import os
 
 
-@pynvim.plugin
+@neovim.plugin
 class TodoAPI:
 
     def __init__(self,nvim):
@@ -15,7 +15,9 @@ class TodoAPI:
         self.set_api_key()
         self.client = NotionClient(token_v2=self.keys["TOKEN_V2"])
         self.page = self.client.get_block(self.keys["PAGE_URL"])
-        print("page=>",self.page)
+
+    def echo(self,msg):
+        self.nvim.command("echo '" + msg + "'")
 
     def set_api_key(self):
         keys=["TOKEN_V2","PAGE_URL"]
@@ -29,20 +31,21 @@ class TodoAPI:
         cur_todos=[child.title for child in self.page.children]
         return cur_todos
 
-    @pynvim.function("AddTodo")
+    @neovim.function("AddTodo",nargs=1)
     def add_new_odo(self,title):
+        self.echo(title)
         if len(title)==0:
             raise Exception("tilte is required")
         else:
             self.pag.children.add_new(TodoBlock,title=title[0])
 
-    @pynvim.function("DeleteTodo")
+    @neovim.function("DeleteTodo")
     def delete_tdo(self,idx):
         print("delete called idx:{}".format(idx))
         child = self.page.children[idx]
         child.remove()
 
-    @pynvim.function("ToggleTodo")
+    @neovim.function("ToggleTodo")
     def toggle_checked(self,idx):
         print("toggle_cheched idx:{}".format(idx))
         child = self.page.children[idx]
