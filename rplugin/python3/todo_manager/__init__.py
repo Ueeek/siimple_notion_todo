12040b292ba7097d8d3b3a97f8c389f21f830e02
Ueeek <ueki.k.ae@m.titech.ac.jp>
2021-12-04 19:50:00 +0900

    fix

--- rplugin/python3/todo_manager/__init__.py
+++ rplugin/python3/todo_manager/__init__.py
@@ -3,60 +3,45 @@ from notion.block import TodoBlock
 from notion.block import PageBlock
 import json
 import pynvim
+import os
+
 
 @pynvim.plugin
-class Main(object):
-    def __init__(self,nvim):
-        self.nvim=nvim
-        self.nvim.command('echo "Hello from python"')
-
-    @pynvim.function("DoItPython",sync=True)
-    def doItPython(self,args):
-        self.nvim.command('echo "Hello from python"')
-
-@pynvim.plugin
-class TodoManager:
+class TodoAPI:
 
     def __init__(self,nvim):
+        self.keys={}
         self.nvim=nvim
-        self.read_json()
-        self.client = NotionClient(token_v2=self.token_v2)
-        self.page = self.client.get_block(self.page_url)
+        self.set_api_key()
+        self.client = NotionClient(token_v2=self.keys["TOKEN_V2"])
+        self.page = self.client.get_block(self.keys["PAGE_URL"])
         print("page=>",self.page)
 
-    def read_json(self,file_path="../../../../../simple_notion_todo_env.json"):
-        with open(file_path) as file:
-            data = json.load(file)
-
-        self.token_v2 = data["token_v2"]
-        self.page_url= data["page_url"]
+    def set_api_key(self):
+        keys={"TOKEN_V2","PAGE_URL"}
+        for key in keys.keys():
+            if os.getenv("NOTION_TODO_{}".format(key)) is None:
+                raise Exception("Required Environment variables are missing")
+            else:
+                self.keys[key]=os.getenv("NOTION_TODO_{}".format(key))
 
     def get_members(self):
         cur_todos=[child.title for child in self.page.children]
-        print(cur_todos)
+        return cur_todos
 
-    @pynvim.function("AddTodo")
-    def add_new_todo(self,title="test"):
+    @pynvim.function("AddTodo",nargs="+")
+    def add_new_todo(self,title):
         print("add called=>",title)
         self.page.children.add_new(TodoBlock,title=title)
 
     @pynvim.function("DeleteTodo")
-    def delete_todo(self,idx=0):
+    def delete_todo(self,idx):
+        print("delete called idx:{}".format(idx))
         child = self.page.children[idx]
         child.remove()
 
     @pynvim.function("ToggleTodo")
-    def toggle_checked(self,idx=0):
+    def toggle_checked(self,idx):
+        print("toggle_cheched idx:{}".format(idx))
         child = self.page.children[idx]
         child.checked = not child.checked
-
-
-
-
-if __name__=="__main__":
-    C = TodoManager()
-    C.add_new_todo("add from class")
-    C.get_members()
-    C.toggle_checked(0)
-    C.delete_todo(0)
-
