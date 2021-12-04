3dc7e75ad4c6c519f60adf36c135174eb07ab095
Ueeek <ueki.k.ae@m.titech.ac.jp>
2021-12-04 22:42:19 +0900

    fix

--- rplugin/python3/todo_manager/__init__.py
+++ rplugin/python3/todo_manager/__init__.py
@@ -70,7 +70,7 @@ class TodoAPI:
             self.page.children.add_new(TodoBlock,title=title[0])
         self.echo("add {}".format(title[0]))
         self.update_list()
-        self.show_list()
+        self.todoList()
 
     @pynvim.command(_command_prefix+"DeleteTodo",nargs=1)
     def delete_todo(self,idx):
@@ -79,7 +79,7 @@ class TodoAPI:
         child.remove()
         self.echo("remove {}".format(removing_title))
         self.update_list()
-        self.show_list()
+        self.todoList()
 
 
     @pynvim.command(_command_prefix+"ToggleTodo",nargs=1)
@@ -87,7 +87,7 @@ class TodoAPI:
         child = self.page.children[int(idx[0])]
         child.checked = not child.checked
         self.update_list()
-        self.show_list()
+        self.todoList()
         self.echo("Toggle {}".format(child.title))
 
     @pynvim.command(_command_prefix+"TodoList")
