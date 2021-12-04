1e77e6e41699cc83a580189ae6782b32891cceab
Ueeek <ueki.k.ae@m.titech.ac.jp>
2021-12-04 22:12:43 +0900

    fix

--- rplugin/python3/todo_manager/__init__.py
+++ rplugin/python3/todo_manager/__init__.py
@@ -90,17 +90,15 @@ class TodoAPI:
         cur_win_id = self.nvim.command('echo win_getid()')
         if cur_win_id==self.view_window_id:
             self.echo("you already todolist")
-            
-            pass
         elif self.view_window_id!=-1:
             self.echo("move to todolist")
             #move to already-opened window
             self.nvim.command('win_gotoid',self.view_window_id)
         else:
-            self.echo("create todolist")
             self.nvim.command('setlocal splitright')
             self.nvim.command('vnew')
             self.nvim.command('vertical resize 30')
             self.nvim.command('setlocal buftype=nofile bufhidden=hide nolist nonumber nomodifiable wrap')
             self.view_window_id = self.nvim.command('echo win_getid()')
+            self.echo("create todolist win_id={}".format(self.view_window_id))
             self.show_list()
